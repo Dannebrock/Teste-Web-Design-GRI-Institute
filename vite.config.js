0b12eb0a34eba6d942f6cb54107ec11cf605fefd
Matheus Dannebrock <dannebroock@gmail.com>
2025-08-08 14:08:58 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-// Troque pelo nome do seu repositório no GitHub
-const repoName = 'Teste-Web-Design-GRI-Institute'
-
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
+  base: '/Teste-Web-Design-GRI-Institute/', // 👈 importante para GitHub Pages
   plugins: [
     vue(),
     tailwindcss(),
